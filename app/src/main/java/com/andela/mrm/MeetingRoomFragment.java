dab3d26b92dd4df0ef9f9bf843523d9a83074b4c
Fred <fredadewole@gmail.com>
2018-04-07 10:16:03 +0100

    feature(Dummy Room Data): add adapter to display dummy data

--- app/src/main/java/com/andela/mrm/MeetingRoomFragment.java
+++ app/src/main/java/com/andela/mrm/MeetingRoomFragment.java
@@ -6,13 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MeetingRoomFragment extends Fragment {
-
     /**
      * Instantiates a new Meeting room fragment.
      */
@@ -23,7 +23,14 @@ public class MeetingRoomFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_meeting_room, container, false);
+        View view = inflater.inflate(R.layout.fragment_meeting_room, container, false);
+
+        GridView gridView = view.findViewById(R.id.meeting_rooms_grid_view);
+        MeetingRoomsAdapter meetingRoomsAdapter = new MeetingRoomsAdapter(getContext(),
+                MeetingRoom.initializeMeetingRooms());
+        gridView.setAdapter(meetingRoomsAdapter);
+
+        return view;
     }
 
 }
